e527eb461f25d2b102c9ec41376fe135aa015d8b
Mike Bridge <mike@bridgecanada.com>
2015-11-07 23:58:58 -0700

    add wiki examples

--- Liquid.NET.Tests/Examples/ExampleTests.cs
+++ Liquid.NET.Tests/Examples/ExampleTests.cs
@@ -27,6 +27,7 @@ namespace Liquid.NET.Tests.Examples
             Assert.That(template.Render(ctx), Is.EqualTo("<div>Hello World</div>"));
 
         }
+
         [Test]
         public void Test_Simple_Collection()
         {
@@ -45,6 +46,41 @@ namespace Liquid.NET.Tests.Examples
 
         }
 
+        [Test]
+        public void Test_Simple_Hash()
+        {
+            ITemplateContext ctx = new TemplateContext();
+            var nameHash = new LiquidHash
+            {
+                {"first", LiquidString.Create("Tobias")},
+                {"last", LiquidString.Create("Lütke")}
+            };
+
+            ctx.DefineLocalVariable("greeting", new LiquidHash
+            {
+                {"address", LiquidString.Create("Hello")},
+                {"name", nameHash}
+            });
+
+            var template = LiquidTemplate.Create("You said '{{ greeting.address }} {{ greeting.name.first }} {{ greeting.name.last }}'");
+
+            Assert.That(template.Render(ctx), Is.EqualTo("You said 'Hello Tobias Lütke'"));
+
+        }
+
+        [Test]
+        public void Test_Filter()
+        {
+            ITemplateContext ctx = new TemplateContext()
+                .WithAllFilters()
+                .DefineLocalVariable("resultcount", LiquidNumeric.Create(42))
+                .DefineLocalVariable("searchterm", LiquidString.Create("MiXeDcAsE"));
+
+            var template = LiquidTemplate.Create("{{ resultcount }} {{ resultcount | pluralize: 'item', 'items' }} were found for '{{searchterm | downcase}}'.");
+
+            Assert.That(template.Render(ctx), Is.EqualTo("42 items were found for 'mixedcase'."));
+
+        }
 
     }
 }
